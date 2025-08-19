552c04fb262a65be417f669b163d31afcfdbf7d0
nigarofe <nigarofe@gmail.com>
2025-08-19 10:55:19 -0300

    sharedHead.html

--- out/webviews/outlinesWebview.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const lineEl = document.getElementById("line-number");
-if (lineEl) {
-    lineEl.textContent = "42";
-}
-//# sourceMappingURL=outlinesWebview.js.map
--- src/webviews/sharedHtmlProvider.ts
+++ src/webviews/sharedHtmlProvider.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import { promises as fs } from "fs";
+import { get } from "http";
 
 const possibleWebviews: Record<string, { htmlFileName: string; scriptFileName:string; title: string ; panel: vscode.WebviewPanel | undefined }> = {
 	"Outlines.txt": {
@@ -37,16 +38,29 @@ export async function openCorrespondingWebview(context: vscode.ExtensionContext,
         mapping.panel.onDidDispose(() => { mapping.panel = undefined; });
     }
 
+    mapping.panel.webview.html = await getHtmlForWebview(fileName, context);
+	mapping.panel.reveal(mapping.panel.viewColumn, true);
+}
+
+async function getHtmlForWebview(fileName: string, context: vscode.ExtensionContext) {
+	const mapping = possibleWebviews[fileName];
+
+	const htmlHeadPath = path.join(context.extensionPath, "src", "webviews", "sharedHead.html");
+	let html = await fs.readFile(htmlHeadPath, 'utf8');
+
 	const htmlPath = path.join(context.extensionPath, "src", "webviews", mapping.htmlFileName);
     const jsPath = path.join(context.extensionPath, "src", "webviews", mapping.scriptFileName);
 
-	let html = await fs.readFile(htmlPath, 'utf8');
+	html += await fs.readFile(htmlPath, 'utf8');
 
+	if(!mapping.panel){return "No mapping panel";};
 	const scriptUri = mapping.panel.webview.asWebviewUri(vscode.Uri.file(jsPath));
 	const csp = `<meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src ${mapping.panel.webview.cspSource}; style-src ${mapping.panel.webview.cspSource} 'unsafe-inline';">`;
 
 	html = html.replace('%%SCRIPT_URI%%', scriptUri.toString()).replace('%%CSP%%', csp);
-    mapping.panel.webview.html = html;
 
-	mapping.panel.reveal(mapping.panel.viewColumn, true);
+	if(fileName === "Outlines.txt"){
+		
+	}
+	return html;
 }
