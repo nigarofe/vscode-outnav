7d073b74c6939ed430d503c672f7cc6e6770f38a
nigarofe <nigarofe@gmail.com>
2025-08-19 11:34:46 -0300

    rename and delete

--- src/webviews/outlinesWebview.js
+++
@@ -1,9 +0,0 @@
-const outlinesWebviewJSONel = document.getElementById("outlinesWebviewJSON");
-
-window.addEventListener("message", (event) => {
-    console.log("Received message in outlinesWebview.js:", event.data);
-    if (event.data.type === "cursorUpdate") {
-        outlinesWebviewJSONel.style.whiteSpace = 'pre-wrap'; // preserve line breaks and wrapping
-        outlinesWebviewJSONel.textContent = JSON.stringify(event.data.payload, null, 2);
-    }
-});
