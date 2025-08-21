2cbde54016d53a1b1ddfb06f9384725de95d388f
nigarofe <nigarofe@gmail.com>
2025-08-21 20:58:28 -0300

    webviews-config.ts

--- src/webviews-config.ts
+++ src/webviews-config.ts
@@ -1,22 +1,44 @@
 import * as vscode from "vscode";
 
-export const possibleWebviews: Record<string, { htmlFileName: string; scriptFileName: string; title: string; panel: vscode.WebviewPanel | undefined }> = {
+export const possibleWebviews: Record<string,
+    {
+        htmlFileName: string; scriptFileName: string; title: string; panel: vscode.WebviewPanel | undefined,
+        spacing: Record<string, number>, required_headings_regex: Record<string, RegExp>
+    }> = {
     "Outlines.txt": {
         htmlFileName: "outlinesWebview.html",
         scriptFileName: "outlinesWebview.js",
         title: "Outlines",
-        panel: undefined
+        panel: undefined,
+        spacing: {
+            "level1": 10,
+            "level2": 2
+        },
+        required_headings_regex: {}
     },
     "Premises.md": {
         htmlFileName: "premisesWebview.html",
         scriptFileName: "premisesWebview.js",
         title: "Premises",
-        panel: undefined
+        panel: undefined,
+        spacing: {
+            "level1": 50,
+            "level2": 10,
+            "level3": 2
+        },
+        required_headings_regex: {}
     },
     "Questions.md": {
         htmlFileName: "questionsWebview.html",
         scriptFileName: "questionsWebview.js",
         title: "Questions",
-        panel: undefined
+        panel: undefined,
+        spacing: {
+            "level1": 10,
+            "level2": 2
+        },
+        required_headings_regex: {
+            "level1": /^#\s+Question\s+(\d+)/gm
+        }
     }
 };
--- src/webviews/sharedScript.js
+++ src/webviews/sharedScript.js
@@ -1,15 +1,11 @@
-// expose renderKatex on the global window so other module scripts can call it
 window.renderKatex = function renderKatex() {
     renderMathInElement(document.body, {
-        // customised options
-        // • auto-render specific keys, e.g.:
         delimiters: [
             { left: '$$', right: '$$', display: true },
             { left: '$', right: '$', display: false },
             { left: '\\(', right: '\\)', display: false },
             { left: '\\[', right: '\\]', display: true }
         ],
-        // • rendering keys, e.g.:
         throwOnError: false
     });
 };
